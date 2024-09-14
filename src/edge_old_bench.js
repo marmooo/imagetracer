17a4418b932b19d2d69c9122b69b14b12420c4e3
marmooo <marmooo@users.noreply.github.com>
2024-09-14 23:44:41 +0900

    cleanup

--- src/edge_old_bench.js
+++ src/edge_old_bench.js
@@ -7,39 +7,41 @@ import {
   detectEdgesByBorderedPalette,
 } from "./edge_old.js";
 
-function createRandomIndexedImage(n) {
-  const indexedImage = new Uint8Array(n * n);
-  for (let i = 0; i < n * n; i++) {
+function createRandomIndexedImage(width, height) {
+  const size = width * height;
+  const indexedImage = new Uint8Array(size);
+  for (let i = 0; i < size; i++) {
     indexedImage[i] = Math.round(Math.random());
   }
   return indexedImage;
 }
 
-const n = 100;
-const indexedImage = createRandomIndexedImage(n);
+const width = 50;
+const height = 100;
+const indexedImage = createRandomIndexedImage(width, height);
 const palette = [0, 1];
 
 Deno.bench("detectEdges", () => {
   const layers = new Array(palette.length);
   for (let k = 0; k < layers.length; k++) {
-    layers[k] = detectEdges(indexedImage, n, n, k);
+    layers[k] = detectEdges(indexedImage, width, height, k);
   }
 });
 Deno.bench("detectEdgesByBordered", () => {
-  const arr = createBorderedArray(indexedImage, n, n);
+  const arr = createBorderedArray(indexedImage, width, height);
   const layers = new Array(palette.length);
   for (let k = 0; k < layers.length; k++) {
     layers[k] = detectEdgesByBordered(arr, k);
   }
 });
 Deno.bench("detectEdgesByBordered8", () => {
-  const arr8 = createBorderedInt8Array(indexedImage, n, n);
+  const arr8 = createBorderedInt8Array(indexedImage, width, height);
   const layers = new Array(palette.length);
   for (let k = 0; k < layers.length; k++) {
-    layers[k] = detectEdgesByBordered8(arr8, n + 2, n + 2, k);
+    layers[k] = detectEdgesByBordered8(arr8, width + 2, height + 2, k);
   }
 });
 Deno.bench("detectEdgesByBorderedPalette", () => {
-  const arr = createBorderedArray(indexedImage, n, n);
+  const arr = createBorderedArray(indexedImage, width, height);
   detectEdgesByBorderedPalette(arr, palette);
 });
