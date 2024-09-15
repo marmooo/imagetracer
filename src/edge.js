31e8446eaba53e9bd8f4182b0eff0c8c6ac3fe2d
marmooo <marmooo@users.noreply.github.com>
2024-09-15 18:11:27 +0900

    cleanup

--- src/edge.js
+++ src/edge.js
@@ -27,10 +27,10 @@ export function detectEdges(
 export function createBorderedInt16Array(uint8, width, height) {
   const bordered = new Int16Array((width + 2) * (height + 2));
   for (let j = 0; j < height; j++) {
-    const indexFrom = j * width;
-    const indexTo = (j + 1) * (width + 2);
+    const yFrom = j * width;
+    const yTo = (j + 1) * (width + 2);
     for (let i = 0; i < width; i++) {
-      bordered[indexTo + i + 1] = uint8[indexFrom + i];
+      bordered[yTo + i + 1] = uint8[yFrom + i];
     }
   }
   for (let i = 0; i < height + 2; i++) {
